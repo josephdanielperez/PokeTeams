98e042af52cc7601fb54beee4d5eb4e9740aa759
Joseph Perez <hello@josephdanielperez.com>
2020-11-03 08:08:24 -0800

    commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,11 @@
+### CREATING INSTANCES OF USERS ###
 u1 = User.create(username:"AshKetchum", email:"ash@pokemon.com", password: "ChampionRed")
 u2 = User.create(username:"GaryOak", email:"gary@pokemon.com", password: "RivalBlue")
 
+### CREATING INSTANCES OF TEAMS ###
 t1 = Team.create(name: "Ash's Team", pokemon_1: "Pikachu", pokemon_2: "Pidgeotto", pokemon_3: "Butterfree", pokemon_4: "Bulbasaur", pokemon_5: "Charizard", pokemon_6: "Squirtle")
 t2 = Team.create(name: "Gary's Team", pokemon_1: "Eevee", pokemon_2: "Blastoise", pokemon_3: "Arcanine", pokemon_4: "Fearow", pokemon_5: "Nidoking", pokemon_6: "Golem")
 
+### SHOVELING TEAMS INTO USER INSTANCES ###
 u1.teams << t1
 u2.teams << t2
