f2671d59b23ebdee67a4135447a314c73f6c1df9
Joseph Perez <hello@josephdanielperez.com>
2020-11-03 08:00:24 -0800

    commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,13 +7,5 @@ class User < ActiveRecord::Base
     validates_uniqueness_of :email
     validates_uniqueness_of :username
     has_secure_password
-
-    def slug
-        username.downcase.split(" ").join("-")
-    end
-
-    def self.find_by_slug(slug)
-        self.all.find_by{|x| x.slug == slug}
-    end
     
 end
